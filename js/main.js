0573e612454f307d734f918b0255f538b258de26
yosi90 <yosi121990@hotmail.es>
2023-02-26 13:17:40 +0000

    cambio de canal Fase 1

--- js/main.js
+++ js/main.js
@@ -15,3 +15,93 @@ $("#botonEncender").click(function () {
     $('#apagado').toggle();
     $('#apagado').toggleClass("actual");
 });
+
+let canalIni = 2;
+$('#cambiaCanal').mousedown(function (event) {
+    switch (event.which) {
+        case 1:
+            switch (canalIni) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;
+            }
+            if (canalIni < 3)
+                canalIni++;
+            break;
+        case 2:
+            // alert('Middle Mouse button pressed.');
+            break;
+        case 3:
+            switch (canalIni) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
+            if (canalIni > 0)
+                canalIni--;
+            break;
+        default:
+            alert('WTF!');
+    }
+});
+
+let volIni = 2;
+$('#cambiaVolumen').mousedown(function (event) {
+    switch (event.which) {
+        case 1:
+            switch (volIni) {
+                case 0:
+                    $('#volumen').toggleClass("fa-volume-xmark");
+                    $('#volumen').toggleClass("fa-volume-off");
+                    break;
+                case 1:
+                    $('#volumen').toggleClass("fa-volume-off");
+                    $('#volumen').toggleClass("fa-volume-low");
+                    break;
+                case 2:
+                    $('#volumen').toggleClass("fa-volume-low");
+                    $('#volumen').toggleClass("fa-volume-high");
+                    break;
+                default:
+                    break;
+            }
+            if (volIni < 3)
+                volIni++;
+            break;
+        case 2:
+            // alert('Middle Mouse button pressed.');
+            break;
+        case 3:
+            switch (volIni) {
+                case 1:
+                    $('#volumen').toggleClass("fa-volume-xmark");
+                    $('#volumen').toggleClass("fa-volume-off");
+                    break;
+                case 2:
+                    $('#volumen').toggleClass("fa-volume-off");
+                    $('#volumen').toggleClass("fa-volume-low");
+                    break;
+                case 3:
+                    $('#volumen').toggleClass("fa-volume-low");
+                    $('#volumen').toggleClass("fa-volume-high");
+                    break;
+                default:
+                    break;
+            }
+            if (volIni > 0)
+                volIni--;
+            break;
+        default:
+            alert('WTF!');
+    }
+});
